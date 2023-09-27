344b329256f2ed48b22f68720426576e18dcd499
dcamm79 <dinhcam1512@gmail.com>
2023-09-27 20:52:46 +0700

    fix

--- realestate/urls.py
+++ realestate/urls.py
@@ -12,9 +12,10 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+try:
+    from .local_settings import *
+except ImportError:
+    pass
