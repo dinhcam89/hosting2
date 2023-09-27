bea5476e932860e98b2ba3700b02a0a5561089d4
dcamm79 <dinhcam1512@gmail.com>
2023-09-27 19:21:20 +0700

    fix

--- realestate/urls.py
+++ realestate/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
 
 if settings.DEBUG:
